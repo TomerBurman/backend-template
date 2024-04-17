feed1fa07b98a2f74e52114883fc0398fa540165
TomerBurman <93316511+TomerBurman@users.noreply.github.com>
2024-04-17 17:04:30 +0300

    added deletion/update by ID to student

--- controllers/student_controller.js
+++ controllers/student_controller.js
@@ -1,16 +1,35 @@
 const Student = require("../models/student_model.js");
 
-const getStudents = (req, res, next) => {
-    res.send("students get");
+const getStudents = async (req, res, next) => {
+    console.log("Get students");
+    let student;
+    try{
+        if (req.query.name){
+            student = await Student.find({name: req.query.name});
+
+        }else{
+            student = await Student.find()
+        }
+        res.status(200).send(student);
+}catch(error){
+    res.status(400).send(error.message);
+
 }
-const getStudentById = (req, res, next) => {
-    res.send("student get by id");
+}
+
+const getStudentById = async (req, res, next) => {
+    console.log("student get by id");
+    try{
+        const student = await Student.findById(req.params.id);
+        res.status(200).send(student);
+    }catch(error){
+        res.status(400).send(error.message);
+    }
 }
 
 const postStudent = async (req, res, next) => {
     console.log(req.body);
     try{
-        console.log()
         const student = await Student.create(req.body);
         console.log(student);
         res.status(201).send(student);
@@ -21,18 +40,31 @@ const postStudent = async (req, res, next) => {
     }
 }
 
-const updateStudent = (req, res, next) => {
-    res.send("student put");
-}
-const updateStudentById = (req, res, next) => {
-    res.send("student put by id");
+// Finds a student by their ID and updates values
+const updateStudent = async (req, res, next) => {
+    console.log(req.body);
+    try{
+        const updatedStudent = await Student.findOneAndUpdate({_id:req.params.id}, req.body, {
+            new: true
+          });
+        res.status(200).send(updatedStudent)
+    }catch(error){
+        res.status(400).send(error.message);
+    }
+
 }
 
-const deleteStudent = (req, res, next) => {
-    res.send("student delete");
-}
-const deleteStudentById = (req, res, next) => {
-    res.send("student delete by id");
+const deleteStudent = async (req, res, next) => {
+    console.log("student delete");
+    try{
+        const deletedStudent = await Student.findOneAndDelete({_id:req.params.id}, req.body, {
+            new:true
+        });
+        res.status(200).send(deletedStudent);
+    }catch(error){
+        res.status(400).send(error.message);
+    }
 }
 
-module.exports = {getStudents, postStudent, updateStudent, deleteStudent, updateStudentById, deleteStudentById, getStudentById}
+
+module.exports = {getStudents, postStudent, updateStudent, deleteStudent, getStudentById}
--- routes/student_route.js
+++ routes/student_route.js
@@ -3,16 +3,21 @@ const router = express.Router();
 const student_controller = require("../controllers/student_controller")
 
 router.get("/", student_controller.getStudents);
+
 router.get("/:id", student_controller.getStudentById);
+
+
 
 
 router.post("/", student_controller.postStudent);
 
 
-router.put("/", student_controller.updateStudent);
-router.put("/:id", student_controller.updateStudentById);
 
 
-router.delete("/", student_controller.deleteStudent);
-router.delete("/:id", student_controller.deleteStudentById);
+router.put("/:id", student_controller.updateStudent);
+
+
+
+
+router.delete("/:id", student_controller.deleteStudent);
 module.exports = router;
